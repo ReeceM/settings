64d9191cb6f4b2fec5aa09c3485cdb4f5b76982a
ReeceM <reecestevenmay@gmail.com>
2019-05-13 00:06:47 +0200

    wip

--- src/Facade.php
+++ src/Facade.php
@@ -4,6 +4,20 @@ namespace ReeceM\Settings;
 
 use Illuminate\Support\Facades\Facade as BaseFacade;
 
+/**
+ * @method public function __construct($mapper = null, Storage $storage = null)
+ * @method public function get($key, $default = null)
+ * @method public function all()
+ * @method public function cache()
+ * @method public function load()
+ * @method public function create($data)
+ * @method public function update($data = null)
+ * @method public function getFresh()
+ * @method public function toJson()
+ * @method public function toArray()
+ * @method public function __toString()
+ */
+
 class Facade extends BaseFacade {
     /**
      * Get the registered name of the component.

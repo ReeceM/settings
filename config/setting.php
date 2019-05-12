3f7f2746dbe1f3817187fef27f182c484d32c86e
ReeceM <reecestevenmay@gmail.com>
2019-05-12 23:42:56 +0200

    refactor comments and encrypt file

--- config/setting.php
+++ config/setting.php
@@ -1,37 +1,43 @@
 <?php
 
 return [
-    /***********************************************
-    |       settings ADMIN USERS
-    |   Deny all users until they are actually setup
+    /**
+     *     settings ADMIN USERS
+     *  Deny all users until they are actually setup
      */
     'admins' => env('SETTING_ADMINS', ''),
 
     /*
-    |   storage settings for now only has the db table name
-    |   
-    */
+     *   storage settings for now only has the db table name
+     *   
+     */
     'storage' => [
         'table' => 'app_settings',
         'disk'  => env('SETTINGS_DISK', 'local'),
+        'method' => env('SETTING_STORAGE_METHOD', 'serialize')
     ],
 
+    /**
+     * Set this to encrypt the settings file as a whole
+     * set to false default for sanity sake and testing
+     */
+    'encrypt_file' => false,
     /*
-    |   add your custom types to this definition and the methods will be auto-detected
-    */
+     *   add your custom types to this definition and the methods will be auto-detected
+     */
     'types' => [
         
     ],
     /* 
-    |   Applications middleware for the settings channels
-    |
-    */
+     *   Applications middleware for the settings channels
+     *
+     */
     'middleware' => [
         'web', 'auth', 'can:settings.admin'
     ],
     /*
-    |  The setting for the path of the settings routes
-    |   you can change this, no forward slashes.
-    */
+     *  The setting for the path of the settings routes
+     *   you can change this, no forward slashes.
+     */
     'path' => 'system'
 ];

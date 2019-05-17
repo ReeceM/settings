f9980870612053a420933053d6fdcfb71bf9adce
ReeceM <reecestevenmay@gmail.com>
2019-05-18 00:20:56 +0200

    feat: tailwind flash

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,5 +1,5 @@
 @if(session('settings.flash'))
-<section style="position: fixed; right: 1rem; top: 3.2rem; z-index: 999">
+{{-- <section style="position: fixed; right: 1rem; top: 3.2rem; z-index: 999">
     <div class="tile is-ancestor" >
         <div class="tile is-parent is-vertical">
             @foreach(session('settings.flash') as $key => $notice)
@@ -11,6 +11,29 @@
             </div>
             @endforeach
         </div>
+    </div>
+</section> --}}
+@php
+$colors = [
+    'warning' => 'bg-orange-100 text-orange-700 border-orange-500',
+    'success' => 'bg-green-100 text-green-700 border-green-500',
+    'info' => 'bg-blue-100 text-blue-700 border-blue-500',
+    'danger' => 'bg-red-100 text-red-700 border-red-500',
+    'primary' => 'bg-teal-100 text-teal-700 border-teal-500',
+    'link' => 'bg-blue-100 text-blue-700 border-blue-500',
+];
+@endphp
+<section style="z-50 fixed right-0 mt-10 mr-10">
+    <div class="flex flex-col" >
+        @foreach(session('settings.flash') as $key => $notice)
+        <div class="flex pb-4">
+            <div class="flash {{ Arr::get([$colors], $key) }} border-l-4 p-4 shadow-lg cursor-pointer animated fadeInDown" role="alert"  onclick="this.remove()">
+                <svg class="float-right" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
+                <p class="font-bold">{{ ucwords($key) }}</p>
+                <p>{{ $notice }}</p>
+            </div>
+        </div>
+        @endforeach
     </div>
 </section>
 @endif
